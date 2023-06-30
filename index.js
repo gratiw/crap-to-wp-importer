74978893cf5805479abac829316ad0e567e35fe5
Iwo Gratkowski <iwo.gratko@gmail.com>
2023-07-01 01:08:01 +0200

    Cleanup2

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var WPAPI = require( 'wpapi' );
+var parser = require('xml2json');
 
 // You must authenticate to be able to POST (create) a post
 var wp = new WPAPI({
@@ -8,15 +9,22 @@ var wp = new WPAPI({
     password: 'secret'
 });
 
-wp.posts().create({
-    // "title" and "content" are the only required properties
-    title: 'Your Post Title',
-    content: 'Your post content',
-    // Post will be created as a draft by default if a specific "status"
-    // is not specified
-    status: 'publish'
-}).then(function( response ) {
-    // "response" will hold all properties of your newly-created post,
-    // including the unique `id` the post was assigned on creation
-    console.log( response.id );
-})
+fs = require('fs');
+
+fs.readFile( './file_to_import/import.xml', function(err, data) {
+    var json = parser.toJson(data);
+    console.log("to json ->", json);
+ });
+
+// wp.posts().create({
+//     // "title" and "content" are the only required properties
+//     title: 'Your Post Title',
+//     content: 'Your post content',
+//     // Post will be created as a draft by default if a specific "status"
+//     // is not specified
+//     status: 'publish'
+// }).then(function( response ) {
+//     // "response" will hold all properties of your newly-created post,
+//     // including the unique `id` the post was assigned on creation
+//     console.log( response.id );
+// });
