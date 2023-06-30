38e69a18a86e3c9ea562242691b68b2a4d90f6c9
root <root@DESKTOP-57JDP7O.localdomain>
2023-06-30 23:06:21 +0200

    List products

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const express = require("express");
+const app = express();
 const mysql = require('mysql');
-
+ 
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -11,3 +13,14 @@ connection.connect((err) => {
     if (err) throw err;
     console.log('Connected to MySQL Server!');
 });
+ 
+app.listen(3000, () => {
+    console.log('Server is running at port 3000');
+});
+
+connection.query('SELECT * from wp_posts WHERE post_type="product"', (err, rows) => {
+    if (err) throw err;
+    console.log('The data from users table are: \n', rows);
+    connection.end();
+});
+ 
