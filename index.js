382c66e065e09a736675e50ea1364959288c1e1e
Iwo Gratkowski <iwo.gratko@gmail.com>
2023-07-02 16:58:32 +0200

    WP import working

--- index.js
+++ index.js
@@ -3,38 +3,66 @@ const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
 
 const api = new WooCommerceRestApi({
   url: "http://favor.local",
-  consumerKey: "ck_613cc14c7c22b7b0e001af56cca4b8102aeef4d8",
-  consumerSecret: "cs_1740bace17e43a82e05463df2f73f40e4d0e1f56",
+  consumerKey: "ck_f930c7a360e2a74846c9decaf163ef35469a97aa",
+  consumerSecret: "cs_7a867b2ea95cd1521c0b434cbbf763142ff30f12",
   version: "wc/v3"
 });
 
 fs = require('fs');
 
 fs.readFile( './file_to_import/import.xml', function(err, data) {
-    var parsedXml = parser.toJson(data);
-    var json = JSON.parse(parsedXml);
-    var product = json.products.product;
-    var counter = 0;
+  
+  var parsedXml = parser.toJson(data);
+  var json = JSON.parse(parsedXml);
+  var product = json.products.product;
+  var counter = 0;
+  
+  product.forEach(element => {
+    counter++;
 
-    product.forEach(element => {
-        counter++;
-        
-        const newProduct = {
-          title: product.title,
-          content: product.description,
-          type: 'product',
-          status: 'publish',
-          meta: {
-            sku: product.sku,
-            weight: product.weight,
-            price: product.price,
-            // Add more custom meta attributes here as needed
-            // PRZERZUCIĆ SIĘ NA WC API!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-          }
-        };
-
-        console.log(element.nazwa_produktu);
+    const attributesArray = element.attributes.attribute;
+    const convertedAttributes = {};
+    
+    attributesArray.forEach(item => {
+      let value = item.attribute_value;
+      if (!isNaN(value) && value.includes(',')) {
+        value = parseFloat(value.replace(',', '.'));
+      }
+      convertedAttributes[item.attribute_name] = value;
+    });
+    
+    //Create a product
+    api.post("products", {
+    
+      name: element.nazwa_produktu, // See more in https://woocommerce.github.io/woocommerce-rest-api-docs/#product-properties
+      type: "simple",
+      regular_price: element.cena_detaliczna_brutto_pln,
+      sku: element.sku,
+      description: element.opis_dlugi_korzysci_html,
+      stock_quantity: element.ilosc,
+      weight: '',
+      dimensions: { length: element.dlugosc, width: element.szerokość, height: element.wysokosc },
+      categories: [ { id: 15, name: 'Uncategorized', slug: 'uncategorized' } ],
+      images: [],
+      attributes: [ { color: 'czarny', producent: element.nazwa_producenta } ],
+      meta_data: [],
+    }).then((response) => {
+  
+      // Successful request
+      console.log("Response Status:", response.status);
+      console.log("Response Headers:", response.headers);
+      console.log("Response Data:", response.data);
+    }).catch((error) => {
+  
+      // Invalid request, for 4xx and 5xx statuses
+      console.log("Response Status:", error.response.status);
+      console.log("Response Headers:", error.response.headers);
+      console.log("Response Data:", error.response.data);
+    }).finally(() => {
+  
+      // Always executed.
     });
 
-
- });
+    console.log(element);
+  });
+});
