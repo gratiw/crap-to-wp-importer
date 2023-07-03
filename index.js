c4ed828f7c4cd27300a172ca3c6aac8cb6b78167
Iwo Gratkowski <iwo.gratko@gmail.com>
2023-07-03 20:33:56 +0200

    Import product attributes

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ const api = new WooCommerceRestApi({
   version: "wc/v3"
 });
 
+function hasOnlyDigitsAndCommas(value) {
+  const pattern = /^[0-9,]+$/;
+  return pattern.test(value);
+}
+
+function hasLettersAndCommas(value) {
+  const pattern = /[a-zA-Z,]/;
+  return pattern.test(value);
+}
+
 fs = require('fs');
 
 fs.readFile( './file_to_import/import.xml', function(err, data) {
@@ -16,22 +26,53 @@ fs.readFile( './file_to_import/import.xml', function(err, data) {
   var json = JSON.parse(parsedXml);
   var product = json.products.product;
   var counter = 0;
+  var productId;
   
   product.forEach(element => {
     counter++;
 
-    const attributesArray = element.attributes.attribute;
-    const convertedAttributes = {};
+    var attributesArray = element.attributes.attribute;
+    var convertedAttributes = [];
+    var attributeArray = [];
+    var attributeObject = {};
+    var attributesIter = 0;
     
     attributesArray.forEach(item => {
+      
       let value = item.attribute_value;
-      if (!isNaN(value) && value.includes(',')) {
-        value = parseFloat(value.replace(',', '.'));
+
+      if(item.attribute_name == 'SKU'){ return; }
+      if(item.attribute_name == 'Produkt długość (cm)'){ return; }
+      if(item.attribute_name == 'Produkt szerokość (cm)'){ return; }
+      if(item.attribute_name == 'Produkt wysokość (cm)'){ return; }
+      
+      if(value == 'Nie dotyczy'){ return; }
+      
+      if(!isNaN(value) && hasOnlyDigitsAndCommas(value)) {
+        value = Array(value.replace(',', '.'));
+      }else if(!isNaN(value) && hasLettersAndCommas(value)){
+        value = value.split(", ");
+      }else{
+        value = [value];
       }
+
       convertedAttributes[item.attribute_name] = value;
+
+      attributeObject = {
+        name: item.attribute_name,
+        position: attributesIter,
+        visible: true,
+        variation: true,
+        options: value,
+      };
+
+      attributeArray.push(attributeObject);
+      attributesIter++;
     });
+
+    // console.log(attributeArray);
     
-    //Create a product
+    // Create a product
     api.post("products", {
     
       name: element.nazwa_produktu, // See more in https://woocommerce.github.io/woocommerce-rest-api-docs/#product-properties
@@ -39,19 +80,26 @@ fs.readFile( './file_to_import/import.xml', function(err, data) {
       regular_price: element.cena_detaliczna_brutto_pln,
       sku: element.sku,
       description: element.opis_dlugi_korzysci_html,
+      short_description: element.opis_krotki_html,
       stock_quantity: element.ilosc,
       weight: '',
-      dimensions: { length: element.dlugosc, width: element.szerokość, height: element.wysokosc },
+      dimensions: { length: element.dlugosc, width: element.szerokosc, height: element.wysokosc },
       categories: [ { id: 15, name: 'Uncategorized', slug: 'uncategorized' } ],
       images: [],
-      attributes: [ { color: 'czarny', producent: element.nazwa_producenta } ],
-      meta_data: [],
+      attributes: attributeArray,
+      // meta_data: [
+      //   {
+      //     key: 'ean',
+      //     value: element.ean, // Replace with the actual EAN number
+      //   },
+      // ]
     }).then((response) => {
   
       // Successful request
       console.log("Response Status:", response.status);
       console.log("Response Headers:", response.headers);
       console.log("Response Data:", response.data);
+      productId = response.data.id;
     }).catch((error) => {
   
       // Invalid request, for 4xx and 5xx statuses
@@ -63,6 +111,54 @@ fs.readFile( './file_to_import/import.xml', function(err, data) {
       // Always executed.
     });
 
-    console.log(element);
+    // async function uploadImagesToMediaLibrary(imageUrls) {
+    //   const uploadedImages = [];
+    
+    //   for (const imageUrl of imageUrls) {
+    //     try {
+    //       const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+    //       const imageData = Buffer.from(response.data, 'binary');
+    
+    //       const mediaResponse = await api.post('media', {
+    //         media: imageData.toString('base64'),
+    //         mime_type: 'image/jpeg', // Modify mime_type according to your image type
+    //         media_type: 'image'
+    //       });
+    
+    //       uploadedImages.push(mediaResponse.data);
+    
+    //       // Add the uploaded image ID to the product's images array
+    //       productData.images.push({
+    //         src: mediaResponse.data.source_url,
+    //         position: uploadedImages.length
+    //       });
+    //     } catch (error) {
+    //       console.log(`Error uploading image from URL: ${imageUrl}`, error);
+    //     }
+    //   }
+    
+    //   return uploadedImages;
+    // }
+    
+    // const imageUrls = [];
+    
+    // uploadImagesToMediaLibrary(imageUrls)
+    //   .then((uploadedImages) => {
+    //     console.log('Uploaded images:', uploadedImages);
+    
+    //     // Create the product with the uploaded images
+    //     api.post('products', productData)
+    //       .then((response) => {
+    //         console.log('Created product:', response.data);
+    //       })
+    //       .catch((error) => {
+    //         console.log('Error creating product:', error.response.data);
+    //       });
+    //   })
+    //   .catch((error) => {
+    //     console.log('Error uploading images:', error);
+    //   });
+
+    // console.log(element);
   });
 });
