3189a72f086110a91fdda8fd1c0c9414c6f91f10
Iwo Gratkowski <iwo.gratko@gmail.com>
2023-07-03 21:12:12 +0200

    Categories creating and optimalization

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var parser = require('xml2json');
+const parser = require('xml2json');
 const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
 
 const api = new WooCommerceRestApi({
@@ -18,64 +18,99 @@ function hasLettersAndCommas(value) {
   return pattern.test(value);
 }
 
-fs = require('fs');
+const fs = require('fs');
 
-fs.readFile( './file_to_import/import.xml', function(err, data) {
-  
-  var parsedXml = parser.toJson(data);
-  var json = JSON.parse(parsedXml);
-  var product = json.products.product;
-  var counter = 0;
-  var productId;
-  
-  product.forEach(element => {
-    counter++;
+fs.readFile('./file_to_import/import.xml', async function(err, data) {
+  if (err) {
+    console.error(err);
+    return;
+  }
 
-    var attributesArray = element.attributes.attribute;
-    var convertedAttributes = [];
-    var attributeArray = [];
-    var attributeObject = {};
-    var attributesIter = 0;
-    
+  const parsedXml = parser.toJson(data);
+  const json = JSON.parse(parsedXml);
+  const products = json.products.product;
+
+  for (const element of products) {
+    const attributesArray = element.attributes.attribute;
+    const attributeArray = [];
+    let attributesIter = 0;
+
     attributesArray.forEach(item => {
-      
-      let value = item.attribute_value;
+      const value = item.attribute_value;
 
-      if(item.attribute_name == 'SKU'){ return; }
-      if(item.attribute_name == 'Produkt długość (cm)'){ return; }
-      if(item.attribute_name == 'Produkt szerokość (cm)'){ return; }
-      if(item.attribute_name == 'Produkt wysokość (cm)'){ return; }
-      
-      if(value == 'Nie dotyczy'){ return; }
-      
-      if(!isNaN(value) && hasOnlyDigitsAndCommas(value)) {
-        value = Array(value.replace(',', '.'));
-      }else if(!isNaN(value) && hasLettersAndCommas(value)){
-        value = value.split(", ");
-      }else{
-        value = [value];
+      if (
+        item.attribute_name === 'SKU' ||
+        item.attribute_name === 'Produkt długość (cm)' ||
+        item.attribute_name === 'Produkt szerokość (cm)' ||
+        item.attribute_name === 'Produkt wysokość (cm)'
+      ) {
+        return;
       }
 
-      convertedAttributes[item.attribute_name] = value;
+      if (value === 'Nie dotyczy') {
+        return;
+      }
 
-      attributeObject = {
+      let options;
+
+      if (!isNaN(value) && hasOnlyDigitsAndCommas(value)) {
+        options = Array(value.replace(',', '.'));
+      } else if (!isNaN(value) && hasLettersAndCommas(value)) {
+        options = value.split(", ");
+      } else {
+        options = [value];
+      }
+
+      const attributeObject = {
         name: item.attribute_name,
         position: attributesIter,
         visible: true,
         variation: true,
-        options: value,
+        options: options,
       };
 
       attributeArray.push(attributeObject);
       attributesIter++;
     });
 
-    // console.log(attributeArray);
-    
-    // Create a product
-    api.post("products", {
-    
-      name: element.nazwa_produktu, // See more in https://woocommerce.github.io/woocommerce-rest-api-docs/#product-properties
+    const categoryList = element.nazwa_kategorii;
+    const categories = categoryList.split(',').map(category => category.trim());
+
+    const existingCategoryIds = [];
+    const productCategories = [];
+
+    for (const categoryName of categories) {
+      try {
+        const response = await api.get('products/categories', { per_page: 100, search: categoryName });
+
+        if (response.data.length > 0) {
+          const categoryId = response.data[0].id;
+          existingCategoryIds.push(categoryId);
+          productCategories.push({ id: categoryId });
+          console.log(`Category "${categoryName}" already exists with ID: ${categoryId}`);
+        } else {
+          const newCategoryData = {
+            name: categoryName,
+            description: 'Category description',
+            // Additional category properties can be added here
+          };
+
+          const newCategoryResponse = await api.post('products/categories', newCategoryData);
+
+          if (newCategoryResponse.data && newCategoryResponse.data.id) {
+            const categoryId = newCategoryResponse.data.id;
+            existingCategoryIds.push(categoryId);
+            productCategories.push({ id: categoryId });
+            console.log(`Category "${categoryName}" created with ID: ${categoryId}`);
+          }
+        }
+      } catch (error) {
+        console.log('Error:', error);
+      }
+    }
+
+    const productData = {
+      name: element.nazwa_produktu,
       type: "simple",
       regular_price: element.cena_detaliczna_brutto_pln,
       sku: element.sku,
@@ -83,82 +118,28 @@ fs.readFile( './file_to_import/import.xml', function(err, data) {
       short_description: element.opis_krotki_html,
       stock_quantity: element.ilosc,
       weight: '',
-      dimensions: { length: element.dlugosc, width: element.szerokosc, height: element.wysokosc },
-      categories: [ { id: 15, name: 'Uncategorized', slug: 'uncategorized' } ],
+      dimensions: {
+        length: element.dlugosc,
+        width: element.szerokosc,
+        height: element.wysokosc
+      },
+      categories: productCategories,
       images: [],
-      attributes: attributeArray,
-      // meta_data: [
-      //   {
-      //     key: 'ean',
-      //     value: element.ean, // Replace with the actual EAN number
-      //   },
-      // ]
-    }).then((response) => {
-  
-      // Successful request
-      console.log("Response Status:", response.status);
-      console.log("Response Headers:", response.headers);
-      console.log("Response Data:", response.data);
-      productId = response.data.id;
-    }).catch((error) => {
-  
-      // Invalid request, for 4xx and 5xx statuses
-      console.log("Response Status:", error.response.status);
-      console.log("Response Headers:", error.response.headers);
-      console.log("Response Data:", error.response.data);
-    }).finally(() => {
-  
-      // Always executed.
-    });
+      attributes: attributeArray
+    };
 
-    // async function uploadImagesToMediaLibrary(imageUrls) {
-    //   const uploadedImages = [];
-    
-    //   for (const imageUrl of imageUrls) {
-    //     try {
-    //       const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
-    //       const imageData = Buffer.from(response.data, 'binary');
-    
-    //       const mediaResponse = await api.post('media', {
-    //         media: imageData.toString('base64'),
-    //         mime_type: 'image/jpeg', // Modify mime_type according to your image type
-    //         media_type: 'image'
-    //       });
-    
-    //       uploadedImages.push(mediaResponse.data);
-    
-    //       // Add the uploaded image ID to the product's images array
-    //       productData.images.push({
-    //         src: mediaResponse.data.source_url,
-    //         position: uploadedImages.length
-    //       });
-    //     } catch (error) {
-    //       console.log(`Error uploading image from URL: ${imageUrl}`, error);
-    //     }
-    //   }
-    
-    //   return uploadedImages;
-    // }
-    
-    // const imageUrls = [];
-    
-    // uploadImagesToMediaLibrary(imageUrls)
-    //   .then((uploadedImages) => {
-    //     console.log('Uploaded images:', uploadedImages);
-    
-    //     // Create the product with the uploaded images
-    //     api.post('products', productData)
-    //       .then((response) => {
-    //         console.log('Created product:', response.data);
-    //       })
-    //       .catch((error) => {
-    //         console.log('Error creating product:', error.response.data);
-    //       });
-    //   })
-    //   .catch((error) => {
-    //     console.log('Error uploading images:', error);
-    //   });
+    try {
+      const response = await api.post("products", productData);
 
-    // console.log(element);
-  });
+      if (response.data && response.data.id) {
+        console.log('Product created: ' + element.nazwa_produktu);
+        // console.log("Response Status:", response.status);
+        // console.log("Response Headers:", response.headers);
+        // console.log("Response Data:", response.data);
+        productId = response.data.id;
+      }
+    } catch (error) {
+      console.log("Error:", error.response.data);
+    }
+  }
 });
