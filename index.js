6162ed7a8aa5fd698f4f27f7011888f9ccddbcf4
Iwo Gratkowski <iwo.gratko@gmail.com>
2023-07-04 22:42:59 +0200

    Console logging stuff and cleanup

--- index.js
+++ index.js
@@ -21,49 +21,62 @@ function hasLettersAndCommas(value) {
   return pattern.test(value);
 }
 
-////////////////////////////////////////////////////////
-async function uploadImagesToWordPress(imageUrls) {
+function uploadImagesToWordPress(imageUrls) {
   const base64Credentials = Buffer.from('admin:secret').toString('base64');
   const authHeader = `Basic ${base64Credentials}`;
 
-  try {
-    const imageArray = [];
-    for (const url of imageUrls) {
-      const response = await uploadImage(url, authHeader);
-      
-      if (response.status === 201) {
-        const imageId = response.data.id;
-        imageArray.push({ id: imageId });
-      }
+  const imageArray = [];
+  let imageIndex = 0;
+
+  function uploadNextImage() {
+    if (imageIndex >= imageUrls.length) {
+      return Promise.resolve(imageArray);
     }
 
-    return imageArray;
-  } catch (error) {
-    console.error('Error uploading images:', error);
-    return [];
+    const url = imageUrls[imageIndex];
+    return uploadImage(url, authHeader)
+      .then(response => {
+        if (response.status === 201) {
+          const imageId = response.data.id;
+          imageArray.push({ id: imageId });
+        }
+        imageIndex++;
+        return uploadNextImage();
+      })
+      .catch(error => {
+        console.error('Error uploading image:', error);
+        imageIndex++;
+        return uploadNextImage();
+      });
   }
+
+  return uploadNextImage();
 }
 
-async function uploadImage(url, authHeader) {
+function uploadImage(url, authHeader) {
   const form = new FormData();
-  const imageStream = await axios.get(url, { responseType: 'stream' });
-  const filename = url.substring(url.lastIndexOf('/') + 1);
 
-  form.append('file', imageStream.data, {
-    filename: filename,
-    contentType: imageStream.headers['content-type'],
-  });
+  return axios
+    .get(url, { responseType: 'stream' })
+    .then(response => {
+      const imageStream = response.data;
+      const filename = url.substring(url.lastIndexOf('/') + 1);
 
-  return axios.post('http://favor.local/wp-json/wp/v2/media', form, {
-    headers: {
-      ...form.getHeaders(),
-      'Authorization': authHeader,
-    },
-  });
+      form.append('file', imageStream, {
+        filename: filename,
+        contentType: response.headers['content-type'],
+      });
+
+      return axios.post('http://favor.local/wp-json/wp/v2/media', form, {
+        headers: {
+          ...form.getHeaders(),
+          'Authorization': authHeader,
+        },
+      });
+    });
 }
-////////////////////////////////////////////////////////
 
-fs.readFile('./file_to_import/import.xml', async function(err, data) {
+fs.readFile('./file_to_import/import.xml', function(err, data) {
   if (err) {
     console.error(err);
     return;
@@ -73,7 +86,14 @@ fs.readFile('./file_to_import/import.xml', async function(err, data) {
   const json = JSON.parse(parsedXml);
   const products = json.products.product;
 
-  for (const element of products) {
+  let productIndex = 0;
+
+  function createNextProduct() {
+    if (productIndex >= products.length) {
+      return;
+    }
+
+    const element = products[productIndex];
     const attributesArray = element.attributes.attribute;
     const attributeArray = [];
     let attributesIter = 0;
@@ -94,106 +114,131 @@ fs.readFile('./file_to_import/import.xml', async function(err, data) {
         let tags = value.split(", ");
         const tagObjects = tags.map(tag => ({ name: tag }));
         productTags.push(...tagObjects);
-      }
-
-      if (value === 'Nie dotyczy') {
-        return;
-      }
-
-      let options;
-
-      if (!isNaN(value) && hasOnlyDigitsAndCommas(value)) {
-        options = Array(value.replace(',', '.'));
-      } else if (!isNaN(value) && hasLettersAndCommas(value)) {
-        options = value.split(", ");
-      } else {
-        options = [value];
-      }
-
-      const attributeObject = {
-        name: item.attribute_name,
-        position: attributesIter,
-        visible: true,
-        variation: true,
-        options: options,
-      };
-
-      attributeArray.push(attributeObject);
-      attributesIter++;
-    });
-
-    const categoryList = element.nazwa_kategorii;
-    const categories = categoryList.split(',').map(category => category.trim());
-
-    const existingCategoryIds = [];
-    const productCategories = [];
-
-    for (const categoryName of categories) {
-      try {
-        const response = await api.get('products/categories', { per_page: 100, search: categoryName });
-
-        if (response.data.length > 0) {
-          const categoryId = response.data[0].id;
-          existingCategoryIds.push(categoryId);
-          productCategories.push({ id: categoryId });
-          console.log(`Category "${categoryName}" already exists with ID: ${categoryId}`);
-        } else {
-          const newCategoryData = {
-            name: categoryName,
-            description: 'Category description',
-            // Additional category properties can be added here
-          };
-
-          const newCategoryResponse = await api.post('products/categories', newCategoryData);
-
-          if (newCategoryResponse.data && newCategoryResponse.data.id) {
-            const categoryId = newCategoryResponse.data.id;
-            existingCategoryIds.push(categoryId);
-            productCategories.push({ id: categoryId });
-            console.log(`Category "${categoryName}" created with ID: ${categoryId}`);
-          }
         }
-      } catch (error) {
-        console.log('Error:', error);
-      }
+        if (value === 'Nie dotyczy') {
+            return;
+          }
+        
+          let options;
+        
+          if (!isNaN(value) && hasOnlyDigitsAndCommas(value)) {
+            options = Array(value.replace(',', '.'));
+          } else if (!isNaN(value) && hasLettersAndCommas(value)) {
+            options = value.split(", ");
+          } else {
+            options = [value];
+          }
+        
+          const attributeObject = {
+            name: item.attribute_name,
+            position: attributesIter,
+            visible: true,
+            variation: true,
+            options: options,
+          };
+        
+          attributeArray.push(attributeObject);
+          attributesIter++;
+        });
+        
+        const categoryList = element.nazwa_kategorii;
+        const categories = categoryList.split(',').map(category => category.trim());
+        
+        const existingCategoryIds = [];
+        const productCategories = [];
+        
+        let categoryIndex = 0;
+        
+        function processNextCategory() {
+          if (categoryIndex >= categories.length) {
+            uploadImagesToWordPress(imageArray)
+              .then(imageArray => {
+
+                let ids = imageArray.map(obj => obj.id);
+                let result = ids.join(', ');
+                console.log('Uploaded image IDs:', result);
+        
+                const productData = {
+                  name: element.nazwa_produktu,
+                  type: "simple",
+                  regular_price: element.cena_detaliczna_brutto_pln,
+                  sku: element.sku,
+                  description: element.opis_dlugi_korzysci_html,
+                  short_description: element.opis_krotki_html,
+                  manage_stock: true,
+                  stock_quantity: element.ilosc,
+                  tags: productTags,
+                  weight: element.waga_produktu,
+                  dimensions: {
+                    length: element.dlugosc,
+                    width: element.szerokosc,
+                    height: element.wysokosc
+                  },
+                  categories: productCategories,
+                  images: imageArray,
+                  attributes: attributeArray
+                };
+        
+                return api.post("products", productData);
+              })
+              .then(response => {
+                console.log('Product created:', element.nazwa_produktu);
+                console.log('No:', productIndex);
+                console.log('------------------------------------------------------');
+                productIndex++;
+                createNextProduct();
+              })
+              .catch(error => {
+                console.error('Error creating product:', error);
+                productIndex++;
+                createNextProduct();
+              });
+        
+            return;
+          }
+        
+          const categoryName = categories[categoryIndex];
+        
+          return api.get('products/categories', { per_page: 100, search: categoryName })
+            .then(response => {
+              if (response.data.length > 0) {
+                const categoryId = response.data[0].id;
+                existingCategoryIds.push(categoryId);
+                productCategories.push({ id: categoryId });
+                console.log(`Category "${categoryName}" already exists with ID: ${categoryId}`);
+              } else {
+                const newCategoryData = {
+                  name: categoryName,
+                  description: 'Category description',
+                  // Additional category properties can be added here
+                };
+        
+                return api.post('products/categories', newCategoryData)
+                  .then(newCategoryResponse => {
+                    if (newCategoryResponse.data && newCategoryResponse.data.id) {
+                      const categoryId = newCategoryResponse.data.id;
+                      existingCategoryIds.push(categoryId);
+                      productCategories.push({ id: categoryId });
+                      console.log(`Category "${categoryName}" created with ID: ${categoryId}`);
+                    }
+                  });
+              }
+            })
+            .then(() => {
+              categoryIndex++;
+              processNextCategory();
+            })
+            .catch(error => {
+              console.log('Error processing category:', error);
+              categoryIndex++;
+              processNextCategory();
+            });
+        }
+        
+        let imageArray = Object.values(element).filter(value => typeof value === 'string' && value.startsWith('https://'));
+        
+        processNextCategory();
     }
-    
-    const imageArray = Object.values(element).filter(value => typeof value === 'string' && value.startsWith('https://'));
-  
-    // console.log(element);
-    
-    uploadImagesToWordPress(imageArray)
-      .then(imageArray => {
 
-        console.log('Uploaded image IDs:', imageArray);
-
-        const productData = {
-          name: element.nazwa_produktu,
-          type: "simple",
-          regular_price: element.cena_detaliczna_brutto_pln,
-          sku: element.sku,
-          description: element.opis_dlugi_korzysci_html,
-          short_description: element.opis_krotki_html,
-          manage_stock: true,
-          stock_quantity: element.ilosc,
-          tags: productTags,
-          weight: element.waga_produktu,
-          dimensions: {
-            length: element.dlugosc,
-            width: element.szerokosc,
-            height: element.wysokosc
-          },
-          categories: productCategories,
-          images: imageArray,
-          attributes: attributeArray
-        };
-  
-        const response = api.post("products", productData);
-  
-        console.log('Product created: ' + element.nazwa_produktu);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  }
+    createNextProduct();
 });
